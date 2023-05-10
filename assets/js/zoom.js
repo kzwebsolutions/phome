6d0d06c315ff05f990369335c35abb28211b26cf
kzwebsolutions <129325168+kzwebsolutions@users.noreply.github.com>
2023-05-09 20:25:22 -0700

    Add files via upload

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-    $('.gallery_pics').click(function(e) {
-      // Change Selector Here
-      $(this).toggleClass('fullscreen');
-    });
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
+  var src = $(this).attr('src');
+  var modal;
+
+  function removeModal() {
+    modal.remove();
+    $('body').off('keyup.modal-close');
+  }
+  modal = $('<div>').css({
+    background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
+    backgroundSize: 'contain',
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    zIndex: '10000',
+    top: '0',
+    left: '0',
+    cursor: 'zoom-out'
+  }).click(function() {
+    removeModal();
+  }).appendTo('body');
+  //handling ESC
+  $('body').on('keyup.modal-close', function(e) {
+    if (e.key === 'Escape') {
+      removeModal();
+    }
   });
+});
